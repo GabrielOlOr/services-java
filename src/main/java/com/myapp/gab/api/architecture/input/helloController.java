50f62342150dad0349ce486505af4a980a2efbea
gabriel <reyfrench12@gmail.com>
2026-04-29 23:13:55 -0500

    image

--- src/main/java/com/myapp/gab/api/architecture/input/helloController.java
+++ src/main/java/com/myapp/gab/api/architecture/input/helloController.java
@@ -1,16 +1,21 @@
 package com.myapp.gab.api.architecture.input;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/myApp")
+@Controller
 public class helloController {
 
     @GetMapping("/greeting")
     public String index() {
         return "¡Hola Mundo desde Java 21, Docker y K8s en GCP!";
+    }
+
+    @GetMapping("/sorpresa")
+    public String sorpresa() {
+        return "forward:/sorpresa.html";
     }
 
 }
