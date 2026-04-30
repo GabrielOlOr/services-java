62c3433d7d81832814054a9c75a86aa561f9c715
gabriel <reyfrench12@gmail.com>
2026-04-29 23:44:43 -0500

    image

--- src/main/java/com/myapp/gab/GabApplication.java
+++ src/main/java/com/myapp/gab/GabApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.myapp.gab")
 public class GabApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GabApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GabApplication.class, args);
+    }
 }
