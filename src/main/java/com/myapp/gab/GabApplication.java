748cd5130fc3f31696b71b67defe58e2c0b0ac05
gabriel <reyfrench12@gmail.com>
2026-04-29 23:56:14 -0500

    image

--- src/main/java/com/myapp/gab/GabApplication.java
+++ src/main/java/com/myapp/gab/GabApplication.java
@@ -3,7 +3,7 @@ package com.myapp.gab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.myapp.gab")
+@SpringBootApplication(scanBasePackages = "com.myapp.gab") // <--- ESTO ES LA CLAVE
 public class GabApplication {
     public static void main(String[] args) {
         SpringApplication.run(GabApplication.class, args);
